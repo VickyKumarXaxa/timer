9b9c1b9cc1ab41f65ce9bb76fd7120868cbc8f97
Vicky Kumar Xaxa <vickykumar835302@gmail.com>
2022-06-23 12:46:24 +0530

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,145 @@
 import 'package:flutter/material.dart';
-import 'package:timer/widgets.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import './widgets.dart';
+import './timer.dart';
+import './timermodel.dart';
+import './settings.dart';
 
-void main() {
-  return runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  final double defaultPadding = 5.0;
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Work Timer',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
       home: TimerHomePage(),
+    );
+  }
+}
+
+class TimerHomePage extends StatelessWidget {
+  final double defaultPadding = 5.0;
+  final CountDownTimer timer = CountDownTimer();
+
+  @override
+  Widget build(BuildContext context) {
+    final List<PopupMenuItem<String>> menuItems = <PopupMenuItem<String>>[];
+    menuItems.add(const PopupMenuItem(
+      value: 'Settings',
+      child: Text('Settings'),
+    ));
+    timer.startWork();
+    return MaterialApp(
+      title: 'My Work Timer',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('My Work Timer'),
+            actions: [
+              PopupMenuButton<String>(
+                itemBuilder: (BuildContext context) {
+                  return menuItems.toList();
+                },
+                onSelected: (s) {
+                  if (s == 'Settings') {
+                    goToSettings(context);
+                  }
+                },
+              )
+            ],
+          ),
+          body: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            final double availableWidth = constraints.maxWidth;
+            return Column(children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: const Color(0xff009688),
+                          text: "Work",
+                          onPressed: () => timer.startWork())),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: const Color(0xff607D8B),
+                          text: "Short Break",
+                          onPressed: () => timer.startBreak(true))),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: const Color(0xff455A64),
+                          text: "Long Break",
+                          onPressed: () => timer.startBreak(false))),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                ],
+              ),
+              Expanded(
+                  child: StreamBuilder(
+                      initialData: TimerModel('00:00', 1),
+                      stream: timer.stream(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        TimerModel timer = snapshot.data;
+                        return Container(
+                            child: CircularPercentIndicator(
+                          radius: availableWidth / 2,
+                          lineWidth: 10.0,
+                          percent: (timer.percent == null) ? 1 : timer.percent,
+                          center: Text(
+                              (timer.time == null) ? '00:00' : timer.time,
+                              style: Theme.of(context).textTheme.headline4),
+                          progressColor: const Color(0xff009688),
+                        ));
+                      })),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: const Color(0xff212121),
+                          text: 'Stop',
+                          onPressed: () => timer.stopTimer())),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: const Color(0xff009688),
+                          text: 'Restart',
+                          onPressed: () => timer.startTimer())),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                ],
+              )
+            ]);
+          })),
     );
   }
 
   void emptyMethod() {}
-}
-
-class TimerHomePage extends StatelessWidget {
-  const TimerHomePage({Key? key}) : super(key: key);
-  final double defaultPadding = 5.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Work Timer'),
-      ),
-      body: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-        final double availableWidth = constraints.maxWidth;
-        return Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                  child: ProductivityButton(
-                      color: const Color(0xff009688),
-                      text: 'Work',
-                      size: 10.0,
-                      onPressed: () {}),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                  child: ProductivityButton(
-                    color: const Color(0xff607d8b),
-                    text: 'Break',
-                    size: 10.0,
-                    onPressed: () {},
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                  child: ProductivityButton(
-                    color: const Color(0xff455a64),
-                    text: "Long Break",
-                    size: 10.0,
-                    onPressed: () {},
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: CircularPercentIndicator(
-                radius: availableWidth / 2,
-                lineWidth: 10.0,
-                percent: 1,
-                center: Text(
-                  '30:00',
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-                progressColor: const Color(0xff009688),
-              ),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                  child: ProductivityButton(
-                    color: const Color(0xff212121),
-                    text: 'Stop',
-                    size: 10.0,
-                    onPressed: () {},
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                  child: ProductivityButton(
-                    color: const Color(0xff009688),
-                    text: 'Restart',
-                    size: 10.0,
-                    onPressed: () {},
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-              ],
-            ),
-          ],
-        );
-      }),
-    );
+  void goToSettings(BuildContext context) {
+    // ignore: avoid_print
+    print('in gotoSettings');
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => SettingsScreen()));
   }
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 
+typedef CallbackSetting = void Function(String, int);
+
 class ProductivityButton extends StatelessWidget {
   final Color color;
   final String text;
-  final double size;
+  double? size;
   final VoidCallback onPressed;
-  const ProductivityButton(
+
+  ProductivityButton(
       {required this.color,
       required this.text,
-      required this.size,
       required this.onPressed,
-      super.key});
-
+      this.size});
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      onPressed: onPressed,
-      color: color,
-      minWidth: size,
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.white),
-      ),
+      child: Text(this.text, style: TextStyle(color: Colors.white)),
+      onPressed: this.onPressed,
+      color: this.color,
+      minWidth: (this.size != null) ? this.size : 0,
+    );
+  }
+}
+
+class SettingsButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final int value;
+  final String setting;
+  final CallbackSetting callback;
+  SettingsButton(
+      this.color, this.text, this.value, this.setting, this.callback);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: Text(this.text, style: TextStyle(color: Colors.white)),
+      onPressed: () => this.callback(this.setting, this.value),
+      color: this.color,
     );
   }
 }
